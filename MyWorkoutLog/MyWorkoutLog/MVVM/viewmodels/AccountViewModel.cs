d8886212601ee266c3f0a638632f3ee348d90b4d
Remy-Amor <105914423@student.swin.edu.au>
2025-11-07 23:53:17 +1100

    Initial submit state. Need to implement: - Commands bound to buttons - All pages - Scrolling capabilities for displaying workouts, exercise lists etc

--- MyWorkoutLog/MyWorkoutLog/MVVM/viewmodels/AccountViewModel.cs
+++ MyWorkoutLog/MyWorkoutLog/MVVM/viewmodels/AccountViewModel.cs
@@ -1,8 +1,52 @@
 using MyWorkoutLog.Core;
+using MyWorkoutLog.Services;
+using System.Windows;
 
-namespace MyWorkoutLog.MVVM.ViewModels;
-
-public class AccountViewModel : ViewModel
+namespace MyWorkoutLog.MVVM.ViewModels
 {
-     
+
+    public class AccountViewModel : ViewModel
+    {
+        private string? _currentUsername;
+        public string? CurrentUsername
+        {
+            get { return _currentUsername; }
+            set
+            {
+                _currentUsername = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+        public AccountViewModel()
+        {
+            LoadFromSession();
+
+            // subscribe to SessionData changes so viewmodel updates
+            SessionData.CurrentUserChanged += OnCurrentUserChanged;
+
+        }
+
+        private void LoadFromSession()
+        {
+            // Initialize CurrentUsername from SessionData
+            if (SessionData.CurrentUser != null)
+            {
+                CurrentUsername = SessionData.CurrentUser.Username;
+            }
+            else
+            {
+                CurrentUsername = "None";
+            }
+        }
+
+        private void OnCurrentUserChanged()
+        {
+            LoadFromSession();
+        }
+
+
+        
+    }
 }
